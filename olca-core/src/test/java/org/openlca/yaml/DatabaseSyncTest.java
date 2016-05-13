c980b877f6709fa95472eecf6331b11c36b7d995
msrocka <michael.srocka@gmail.com>
2016-05-13 09:45:56 +0200

    added sync tests for flows and flow properties

--- olca-core/src/test/java/org/openlca/yaml/DatabaseSyncTest.java
+++ olca-core/src/test/java/org/openlca/yaml/DatabaseSyncTest.java
@@ -8,8 +8,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openlca.core.Tests;
+import org.openlca.core.database.FlowDao;
+import org.openlca.core.database.FlowPropertyDao;
 import org.openlca.core.database.IDatabase;
 import org.openlca.core.database.UnitGroupDao;
+import org.openlca.core.model.Flow;
+import org.openlca.core.model.FlowProperty;
 import org.openlca.core.model.UnitGroup;
 
 public class DatabaseSyncTest {
@@ -34,5 +38,25 @@ public class DatabaseSyncTest {
 		List<UnitGroup> list = dao.getAll();
 		Assert.assertEquals(1, list.size());
 		Assert.assertEquals("Units of mass", list.get(0).getName());
+	}
+
+	@Test
+	public void testFlowProperty() {
+		FlowPropertyDao dao = new FlowPropertyDao(db);
+		List<FlowProperty> list = dao.getAll();
+		Assert.assertEquals(1, list.size());
+		FlowProperty p = list.get(0);
+		Assert.assertEquals("Mass", p.getName());
+		Assert.assertEquals("Units of mass", p.getUnitGroup().getName());
+	}
+
+	@Test
+	public void testFlow() {
+		FlowDao dao = new FlowDao(db);
+		List<Flow> list = dao.getAll();
+		Assert.assertEquals(2, list.size());
+		Flow f = list.get(0);
+		Assert.assertEquals("Mass", f.getReferenceFactor()
+				.getFlowProperty().getName());
 	}
 }
